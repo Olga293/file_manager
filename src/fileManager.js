01e293e5637a86242c70299fc1e429d0240be50f
Оля Брикса <oly.briksa@gmail.com>
2024-02-09 14:27:31 +0300

    feat: add message in case of incorrect input

--- src/fileManager.js
+++ src/fileManager.js
@@ -1,10 +1,20 @@
-
-
 export const executCommand = async (userInput) => {
     const [operationType, ...args] = userInput.trim().split(/\s+/g); 
     
-    
     console.log(operationType);
     
+    try{
+        switch (operationType){
+            case 'test':
+                console.log('Good');
+                break;
 
-  }
+            default:
+                console.log('Invalid input');
+        }
+
+    }catch{
+        console.log('Operation failed');
+    }
+
+}
